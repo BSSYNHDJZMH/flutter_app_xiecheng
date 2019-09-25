634b5e18cb957f4dfad576cf21d417fda9cbf4a3
zhaoomaohan <569334386@qq.com>
2019-09-25 16:03:09 +0800

    Flutter基础实战：APP导航框架与常用功能实现 之 轮播图Banner功能开发

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_swiper/flutter_swiper.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 class _HomePageState extends State<HomePage>{
-  final PageController _controller = PageController(
-    initialPage: 0,
-
-  );
+  List _imageUrls = [
+    'https://pages.c-ctrip.com/commerce/promote/car/app/201908/pc/images/image1.png',
+    'https://pages.c-ctrip.com/hotel/201908/fengyeji/fengyejiPC.jpg',
+    'https://dimg04.c-ctrip.com/images/3009180000013phufC273.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context,int index){
+                  return Image.network(
+                    _imageUrls[index],
+                    fit:BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
